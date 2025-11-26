21a6212e21dbb51b8b6eb5ec659985891a8bed2e
eg-bru <egbru2005@gmail.com>
2025-11-26 15:53:37 +0300

    registered models in admin panel and created superuser

--- sports/admin.py
+++ sports/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('home_team', 'away_team', 'score_home', 'score_away', 'status')
+    list_filter = ('status', 'tournament')
+    #admin password: labspass
+
+admin.site.register(Sport)
+admin.site.register(Team)
+admin.site.register(Tournament)
+admin.site.register(Athlete)
+admin.site.register(Article)
+admin.site.register(Tag)
